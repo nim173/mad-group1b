02d98a1b9e93f62b944f4fa459567e56e474bcf5
nkw97 <nkwijewardena@gmail.com>
2019-08-05 10:27:22 +0530

    adding my interfaces

---
+++ app/src/main/java/com/example/pastpaperportal_group1b/Comments_Image.java
@@ -0,0 +1,32 @@
+package com.example.pastpaperportal_group1b;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.pastpaperportal_group1b.ui.main.SectionsPagerAdapter;
+
+public class Comments_Image extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments__image);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+}
--- app/src/main/java/com/example/pastpaperportal_group1b/SignUp.java
+++ app/src/main/java/com/example/pastpaperportal_group1b/SignUp.java
@@ -6,21 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 
-    public void onClick(View view){
-        Intent intent = new Intent(this, SearchCommon.class);
-        startActivity(intent);
-    }
-
-    public void toSignUp(View view){
-        Intent intent = new Intent(this, SignUp.class);
+    public void toComments_Images(View view){
+        Intent intent = new Intent(this, Comments_Image.class);
         startActivity(intent);
     }
 }
